2d1a7939c70c1fc008a4dd3c9cbc9e90a43a9201
Matthew7991 <Matthew7991@gmx.de>
2023-09-08 16:53:25 +0200

    data stored in local file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express"
 import cors from "cors"
+import { readFileSync, existsSync, writeFileSync } from "fs"
 
 const port = 9000
 const server = express()
 
 const guestBookUrl = "/api/guestbook"
-const guestBookEntries = []
+const dataGuestBook = "./guestbook.json"
+
+if (!existsSync(dataGuestBook)) {
+  writeFileSync(dataGuestBook, "[]", "utf8")
+}
+const guestBookEntries = JSON.parse(readFileSync(dataGuestBook))
 
 server.use(cors())
 server.use(express.json())
@@ -17,11 +23,13 @@ server.use((req, res, next) => {
 // guestBookEntrie Example = {firstName: "Ulrike", surName: "Schmidt", email: "mike@ekim.de", message: "Hallo.", id: crypto.randomUUID()}
 
 server.get(guestBookUrl, (_, res) => {
+  console.log(guestBookEntries)
   return res.end(JSON.stringify(guestBookEntries))
 })
 server.post(guestBookUrl, (req, res) => {
   guestBookEntries.push(req.body)
   console.log(guestBookEntries)
+  writeFileSync(dataGuestBook, JSON.stringify(guestBookEntries), "utf8")
   res.end()
 })
 
