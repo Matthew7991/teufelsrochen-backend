174e0cedc3d27a8fe8a44132052fa3a4951dc0aa
Matthew7991 <Matthew7991@gmx.de>
2023-09-11 14:39:44 +0200

    data validation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import cors from "cors"
+import multer from "multer"
+import { validationResult, checkSchema } from "express-validator"
 import { readFileSync, existsSync, writeFileSync } from "fs"
 
 const port = 9000
 const server = express()
+const upload = multer()
 
 const guestBookUrl = "/api/guestbook"
 const dataGuestBook = "./guestbook.json"
@@ -12,6 +15,26 @@ if (!existsSync(dataGuestBook)) {
   writeFileSync(dataGuestBook, "[]", "utf8")
 }
 const guestBookEntries = JSON.parse(readFileSync(dataGuestBook))
+
+const guestBookSchema = {
+  firstName: {
+    errorMessage: "kein Name",
+    notEmpty: true,
+  },
+  lastName: {
+    errorMessage: "kein Name",
+    notEmpty: true,
+  },
+  email: {
+    errorMessage: "email error",
+    notEmpty: true,
+    isEmail: true,
+  },
+  message: {
+    errorMessage: "no message",
+    notEmpty: true,
+  },
+}
 
 server.use(cors())
 server.use(express.json())
@@ -23,14 +46,25 @@ server.use((req, res, next) => {
 // guestBookEntrie Example = {firstName: "Ulrike", surName: "Schmidt", email: "mike@ekim.de", message: "Hallo.", id: crypto.randomUUID()}
 
 server.get(guestBookUrl, (_, res) => {
-  console.log(guestBookEntries)
+  // console.log(guestBookEntries)
   return res.end(JSON.stringify(guestBookEntries))
 })
-server.post(guestBookUrl, (req, res) => {
-  guestBookEntries.push(req.body)
-  console.log(guestBookEntries)
-  writeFileSync(dataGuestBook, JSON.stringify(guestBookEntries), "utf8")
-  res.end()
-})
+server.post(
+  guestBookUrl,
+  upload.none(),
+  checkSchema(guestBookSchema),
+  (req, res) => {
+    const result = validationResult(req)
+
+    if (result.errors.length > 0) {
+      res.status(401).end()
+      return
+    }
+    guestBookEntries.push(req.body)
+    // console.log(guestBookEntries)
+    writeFileSync(dataGuestBook, JSON.stringify(guestBookEntries), "utf8")
+    res.end()
+  }
+)
 
 server.listen(port, () => console.log("Server is running at port", port))
